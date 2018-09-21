c214543225640c52de82ec7afbc6024be9052c00
Ari Fogel <ari@fogelti.me>
2018-09-21 10:45:29 -0700

    Jackson (de)serializable NameRegexNodeSpecifier

--- projects/batfish-common-protocol/src/main/java/org/batfish/specifier/NodeSpecifier.java
+++ projects/batfish-common-protocol/src/main/java/org/batfish/specifier/NodeSpecifier.java
@@ -1,8 +1,10 @@
 package org.batfish.specifier;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import java.util.Set;
 
 /** An abstract specification of a set of nodes in the network. */
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, property = "class")
 public interface NodeSpecifier {
   /**
    * Resolve this specifier into a set of concrete node names.
